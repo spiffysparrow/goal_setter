b54f42cec0e57464126642c757428d5af2732b2e
spiffysparrow <student@appacademy.io>
2015-11-23 17:23:56 -0800

    recreated users controller and application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # Expose these methods to the views
+  helper_method :current_user
+
+  private
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def sign_in!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def sign_out!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,5 @@ class Goal < ActiveRecord::Base
   validates :title, :user_id, :private, presence: true
 
   belongs_to :user
+
 end
